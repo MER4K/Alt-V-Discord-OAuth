e9279180d692817376ea4ab3abc0f809292d5a30
Ricardo Negreiros <75104283+MER4K@users.noreply.github.com>
2021-10-02 16:37:34 -0300

    Add

--- resources/example/client/html/discord/app.js
+++ resources/example/client/html/discord/app.js
@@ -4,6 +4,19 @@ function displayName(name)
 {
 	playerName = name;	
 	document.write(playerName)
+	
+	// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = playerName;
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
 }
 
 window.addEventListener('keydown', key => {
